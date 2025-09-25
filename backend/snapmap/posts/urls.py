4f66fb7a06bd0b4bd9dabe9d7abc8751fc528d2b
Mikhail Yakubenko <muhyrla@gmail.com>
2025-09-25 10:44:22 +0600

    changing backend to kotlin

--- backend/snapmap/posts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'posts'
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('api/user-data/', views.api_user_data, name='api_user_data'),
-]
--- backend/snapmap/quests/validation.py
+++
@@ -1,8 +0,0 @@
-from transformers import pipeline
-
-clf = pipeline("zero-shot-image-classification",
-               model="philschmid/clip-zero-shot-image-classification")
-
-res = clf("test.jpg", candidate_labels=["a photo of a goat", "a photo of an person", "a photo of a dog", "none of the above"])
-
-print(res)
