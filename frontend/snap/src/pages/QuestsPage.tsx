f8c50cb659a9783a05d26b351a14ea856ff0219f
soldatnapasa <soldatnapasa@gmail.com>
2025-12-03 23:33:12 +0600

    фикс квестов
    ганджу барсик

--- frontend/snap/src/pages/QuestsPage.tsx
+++ frontend/snap/src/pages/QuestsPage.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Header } from '../components/Header';
 import { Quest, Difficulty } from '../components/Quests';
 import { TimerBar } from '../components/TimerBar';
+import { tab } from '@testing-library/user-event/dist/tab';
 
 type Tab = 'daily' | 'weekly' | 'special';
 type QuestItem = { id: string; title: string; points: number; difficulty: Difficulty; tab: Tab };
@@ -13,12 +14,14 @@ const DATA: QuestItem[] = [
   { id:'4', title:'желтый цветок', points:150, difficulty:'weekly', tab:'weekly' },
 
   { id:'5', title:'свиристели', points:115, difficulty:'daily', tab:'daily' },
-  { id:'6', title:'знак пешеходного...', points:115, difficulty:'daily', tab:'daily' },
+  { id:'6', title:'знак пешеходного', points:115, difficulty:'daily', tab:'daily' },
   { id:'7', title:'колесо обозрения', points:208, difficulty:'daily', tab:'daily' },
   { id:'8', title:'галоша', points:152, difficulty:'daily', tab:'daily' },
+  { id:'9', title: 'МИШИН НОСЯРА', points:0.5, difficulty:'weekly', tab:'daily' },
 
-  { id:'9', title:'скурагов гег', points:315, difficulty:'special', tab:'special' },
-  { id:'10', title:'беляшка гег', points:315, difficulty:'special', tab:'special' },
+  { id:'10', title:'скурагов гег', points:315, difficulty:'special', tab:'special' },
+  { id:'11', title:'беляшка гег', points:315, difficulty:'special', tab:'special' },
+  { id:'12', title:'бутылка карачинской', points:777, difficulty:'special', tab:'special'},
 ];
 
 export default function QuestsPage() {
